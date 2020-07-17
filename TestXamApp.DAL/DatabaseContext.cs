e6c47f1918d1eecb0853ae8b695d924991e943bb
maxvonlancaster <windchange7@gmail.com>
2020-07-17 20:25:09 +0300

    Entities created

---
+++ TestXamApp.DAL/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestXamApp.DAL.Entities;
+
+namespace TestXamApp.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly string _databasePath;
+
+        protected DatabaseContext(string databasePath)
+        {
+            _databasePath = databasePath;
+
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlite($"Filename={_databasePath}");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+
+
+    }
+}
---
+++ TestXamApp.DAL/Entities/Match.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestXamApp.DAL.Entities
+{
+    public class Match
+    {
+    }
+}
