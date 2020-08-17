95e3f2fa907ee650fa2615c72c6fd8957a03c269
maxvonlancaster <windchange7@gmail.com>
2020-08-17 20:18:42 +0300

    repository started

---
+++ TestXamApp.DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestXamApp.DAL.Entities;
+
+namespace TestXamApp.DAL.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Create(User user);
+        Task Delete(int id);
+        Task Edit(User user);
+        //Task<User> Get(int id);
+        Task<User> GetByCredentials(string userName, string password);
+        Task<List<User>> GetAll();
+        Task<User> GetByUserName(string userName);
+        bool UserExists(string userName);
+    }
+}
---
+++ TestXamApp.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestXamApp.DAL.Entities;
+using TestXamApp.DAL.Repositories.Interfaces;
+
+namespace TestXamApp.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Edit(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByCredentials(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExists(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
